96e32752b1e2964c1fcd64e146a4a770fa4e4d0a
Raghav Agrawal <raghav.github69@gmail.com>
2025-12-24 23:33:03 +0530

    Setuped inital docker Compose for neo4j  container

--- backend/src/main/java/org/example/backend/BackendApplication.java
+++ backend/src/main/java/org/example/backend/BackendApplication.java
@@ -3,11 +3,14 @@ package org.example.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.ai.autoconfigure.openai.OpenAiAutoConfiguration.class,
+        org.springframework.ai.autoconfigure.ollama.OllamaAutoConfiguration.class
+})
 public class BackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
-    }
 
+    }
 }
