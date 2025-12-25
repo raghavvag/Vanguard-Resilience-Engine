674ff4cb63bc4ce73388caa1b6478716db5dfc1a
Raghav Agrawal <raghav.github69@gmail.com>
2025-12-25 18:27:31 +0530

    feat(security): establish stateless security baseline and disable default authentication mechanisms
    - Added explicit SecurityFilterChain configuration to fully control Spring Security behavior
    - Disabled default form login and HTTP basic authentication
    - Configured stateless session management in preparation for JWT-based authentication
    - Disabled CSRF protection for REST-based stateless APIs
    - Defined authorization rules to allow health and actuator endpoints while securing all other requests
    - Laid the foundation for future JWT filter integration without coupling authentication logic

---
+++ backend/src/main/java/org/example/backend/config/security/SecurityConfig.java
@@ -0,0 +1,34 @@
+package org.example.backend.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(csrf->csrf.disable())
+                .formLogin(form->form.disable())
+                .sessionManagement(session->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+
+
+
+                .authorizeHttpRequests(auth->auth
+                        .requestMatchers("/acutator/**").permitAll()
+                        .requestMatchers(".health/**").permitAll()
+                        .anyRequest().authenticated()
+
+
+                );
+        return http.build();
+    }
+}
